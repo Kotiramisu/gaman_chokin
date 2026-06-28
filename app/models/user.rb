691491828afeceabbca52beaa555cac699c7fc60
Yuki <yuuki.yuu.2.6@gmail.com>
2026-06-28 18:26:04 +0900

    Fix user validation formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
+  has_secure_password
 
-    validates :name, presence: true, length: { maximum: 10}
-    validates :email, presence: true, uniqueness: true
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :email, presence: true, uniqueness: true
 end
