284c42da6f2f9ed55c33b73aac0649a724ab416a
Yuki <yuuki.yuu.2.6@gmail.com>
2026-07-09 17:18:22 +0900

    ログインページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 10 }
   validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
