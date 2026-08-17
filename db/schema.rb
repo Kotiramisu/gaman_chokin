e90d74b3b2370b42abf0c2045fd2d217d90106dc
Yuki <yuuki.yuu.2.6@gmail.com>
2026-08-17 13:34:54 +0900

    add:goal_amoutDB作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_29_054927) do
+ActiveRecord::Schema[7.2].define(version: 2026_08_16_161514) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_29_054927) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "goal_amount"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
